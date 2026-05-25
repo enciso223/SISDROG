d53635c6c1387f8a2100811466192a354ae5332e
enciso223 <129301479+enciso223@users.noreply.github.com>
2026-05-25 12:51:47 -0500

    Correcciones configuracion inicial

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
-from app.modules.routes import router as medicines_router
-
 
 app = FastAPI(
     title="Pharmacy Management System",
     version="1.0.0"
 )
 
-app.include_router(medicines_router)
-
-
 @app.get("/")
 def root():
-    return {"message": "Pharmacy API running successfully"}
+    return {
+        "message": "Pharmacy API running successfully"
+    }
