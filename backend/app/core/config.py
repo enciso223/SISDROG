f0680aad70d85020cdfd072b4bf00451198c6716
enciso223 <129301479+enciso223@users.noreply.github.com>
2026-05-26 17:51:42 -0500

    Inicializacion Alembic, configuracion inicial de Alembic y modifiaciones a los requerimientos para gestion de contrasenas

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,16 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
     DATABASE_URL: str
-
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_HOST: str
     POSTGRES_PORT: int
 
-    class Config:
-        env_file = ".env"
+    # The correct Pydantic V2 syntax
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8", 
+        extra="ignore"
+    )
 
 settings = Settings()
